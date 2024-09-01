db0bc5cd61bd7c5d0fc8efd1916702f7348336fd
Steve <stevie.shields@hotmail.co.uk>
2024-09-01 18:38:35 +0100

    Built the four vertical lones on the hero section (color tbc)

--- app/Components/Sections/HeroSection/HeroSection.tsx
+++ app/Components/Sections/HeroSection/HeroSection.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function HeroSection() {
+    const arr = Array(4).fill(0);
   return (
-    <div>HeroSection</div>
-  )
+     <section className="relative h-full w-full">
+        <div className="absolute w-full h-[100vh] top-0 flex justify-evenly z-[-1]">
+      {arr.map((_, i) =>{
+        return (
+          <div key={i} className="relative w-[1px] h-full bg-orange-500">
+            <span className="absolute bottom-0 w-2 h-2 bg-colour-2 left-0 translate-x-[-50%] rounded-full"></span>
+          </div>
+        );
+      })}
+    </div>
+    </section>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
