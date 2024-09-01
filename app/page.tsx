1858c3cbd41bace4c7c2d00ce727db08fcbfe829
Steve <stevie.shields@hotmail.co.uk>
2024-09-01 17:55:30 +0100

    Basic colour set up (change later down the line)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import HeroSection from "./Components/Sections/HeroSection/HeroSection";
+
 export default function Home() {
-  return <main className="bg-2">
-    <h1>Hello! This will hopefully be the Marginal Records website!</h1>
+  return <main>
+    <HeroSection />
   </main>;
 }
