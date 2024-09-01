4817ea91aa89e035e6f981c62f90ec5939dd94ec
Steve <stevie.shields@hotmail.co.uk>
2024-09-01 17:34:43 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        1: "var(--colour--bg-1)",
+        2: "var(--colour--bg-2)",
+        3: "var(--colour--bg-3)",
+        4: "var(--colour--bg-4)",
+        "colour-1": "var(--colour--border-1)",
+        "colour-2": "var(--colour--border-2)",
+      },
     },
   },
   plugins: [],
